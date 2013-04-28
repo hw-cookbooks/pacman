b6567b8d7b6a1d368c7d25f4f51a326a1d68cb55
Sean Escriva <sean.escriva@gmail.com>
2013-04-28 01:35:40 -0700

    Pacman AUR tarball locations have moved

--- providers/aur.rb
+++ providers/aur.rb
@@ -38,7 +38,7 @@ action :build do
 
     Chef::Log.debug("Retrieving source for #{new_resource.name}")
     r = remote_file "#{new_resource.builddir}/#{new_resource.name}.tar.gz" do
-      source "http://aur.archlinux.org/packages/#{new_resource.name}/#{new_resource.name}.tar.gz"
+      source "http://aur.archlinux.org/packages/#{new_resource.name[0,2]}/#{new_resource.name}/#{new_resource.name}.tar.gz"
       owner "root"
       group "root"
       mode 0644
